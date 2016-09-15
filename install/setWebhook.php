dfab80a19f5de523326b9b278222db54e8bf8c14
xiplus <huangxuanyuxiplus@gmail.com>
2016-09-15 17:29:11 +0800

    auto setWebhook

---
+++ install/setWebhook.php
@@ -0,0 +1,6 @@
+<?php
+require(__DIR__."/../config/config.php");
+$commend = 'curl https://api.telegram.org/bot'.$cfg["token"].'/setWebhook -d "url='.urlencode($cfg["webhook"]).'"';
+echo $commend;
+system($commend);
+?>
